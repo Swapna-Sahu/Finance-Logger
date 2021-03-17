8df4f25f87840b62fd6c4c8534132022a32bf9cb
Swapna <swapna.sahu85@gmail.com>
2021-03-17 11:41:52 +0100

    Typescript sample project

---
+++ src/classes/Payments.ts
@@ -0,0 +1,18 @@
+import { HasFormatter } from '../interfaces/HasFormatter';
+
+
+export class Payment implements HasFormatter{
+    // readonly client: string;
+    // private details: string;
+    // public amount: number;
+  
+    constructor(
+      readonly recipient: string, 
+      private details: string, 
+      public amount: number,
+    ){}
+  
+    format() {
+      return `${this.recipient}is owes £${this.amount} for ${this.details}`;
+    }
+  }
